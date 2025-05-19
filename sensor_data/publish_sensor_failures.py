1cff157c35f8a6eb8b3d6660d11410eb003af757
Rene Petersen <rene.petersen@neurospace.io>
2025-05-19 15:40:25 +0200

    Add basic publisher

---
+++ sensor_data/publish_sensor_failures.py
@@ -0,0 +1,19 @@
+import argparse
+
+from publisher import CSVRecordPublisher
+
+def main():
+    parser = argparse.ArgumentParser(prog="publish_sensor_failures")
+    parser.add_argument("-T", "--period", type=int)
+    args = parser.parse_args()
+
+    publisher = CSVRecordPublisher(
+        queue_name="failures",
+        filename="data/predictive_maintenance.csv",
+        cols=["UDI", "Target"]
+    )
+
+    publisher.run(period=args.period)
+
+main()
+
---
+++ sensor_data/publish_sensor_readings.py
@@ -0,0 +1,18 @@
+import argparse
+
+from publisher import CSVRecordPublisher
+
+def main():
+    parser = argparse.ArgumentParser(prog="publish_sensor_readings")
+    parser.add_argument("-T", "--period", type=int)
+    args = parser.parse_args()
+
+    publisher = CSVRecordPublisher(
+        queue_name="readings",
+        filename="data/predictive_maintenance.csv",
+        cols=["UDI", "Air temperature [K]", "Process temperature [K]", "Rotational speed [rpm]", "Torque [Nm]"]
+    )
+
+    publisher.run(period=args.period)
+
+main()
